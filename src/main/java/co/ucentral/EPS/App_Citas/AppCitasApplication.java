28ff63dfd26784b58de374882068a814a2b6186b
Quiz <nfonsecaf2@ucentral.edu.co>
2024-04-16 18:51:37 -0500

    Principio

---
+++ src/main/java/co/ucentral/EPS/App_Citas/AppCitasApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.EPS.App_Citas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/ucentral/EPS/App_Citas/AppCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucentral.EPS.App_Citas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
