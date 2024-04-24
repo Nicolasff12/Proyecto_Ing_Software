090425f248e2a6ceca6864e43044de8c35157eaa
NFF0928 <nicolas.fonseca0928@gmail.com>
2024-04-24 11:14:12 -0500

    Se crea el controlador inicio y los inicios de medico y afiliado

---
+++ src/main/java/co/ucentral/EPS/App_Citas/controladores/ControladorInicio.java
@@ -0,0 +1,45 @@
+package co.ucentral.EPS.App_Citas.controladores;
+
+
+import co.ucentral.EPS.App_Citas.servicios.ServicioPersona;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+@Log4j2
+@Controller
+public class ControladorInicio {
+
+    private static final Logger logger = LogManager.getLogger(ControladorInicio.class);
+
+    @Autowired
+    ServicioPersona servicioPersona;
+
+    @GetMapping({ "/index"})
+    public String Inicio(Model model){
+        //logger.info("Verificando ");
+        //model.addAttribute("index",servicioPersona.obtenerPersona());
+        return "index";
+    }
+
+    @GetMapping({ "/inicioAfiliado"})
+    public String accesoAfiliado(Model model){
+        //logger.info("Verificando ");
+        //model.addAttribute("index",servicioPersona.obtenerPersona());
+        return "inicioAfiliado";
+    }
+
+    @GetMapping({ "/inicioMedico"})
+    public String accesoMedico(Model model){
+        //logger.info("Verificando ");
+        //model.addAttribute("index",servicioPersona.obtenerPersona());
+        return "inicioMedico";
+    }
+
+
+}
